aa108e9db0964a2ce2e8747c5e55e706d3d10802
twinkal patel <twinklepatel491@gmail.com>
2023-02-14 22:05:24 +0000

    java1 file added

---
+++ Java1.java
@@ -0,0 +1,14 @@
+package javatopic;
+//the first package name is java and rename it as java topic
+public class Java1 {
+    /* create class as JavaTopic and rename it as java1 */
+
+        public static void main(String[] args){
+            System.out.println("Write 5 statement about the java");
+            System.out.println("Java is a object oriented programing language");
+            System.out.println("Java is platform independent");
+            System.out.println("Java is designed for distributed environment");
+            System.out.println("Java virtual machine");
+            System.out.println("Java run time environment");
+    }
+}
